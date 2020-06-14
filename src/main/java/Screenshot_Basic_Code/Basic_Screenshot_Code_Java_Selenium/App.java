6e2c4f3d81df3f3cee29fa247a848a5db42b298d
shoaibhannure <shoaib_hannure@yahoo.com>
2020-06-15 00:13:59 +0530

    Screenshot Code Java With Selenium For Begineers

---
+++ src/main/java/Screenshot_Basic_Code/Basic_Screenshot_Code_Java_Selenium/App.java
@@ -0,0 +1,13 @@
+package Screenshot_Basic_Code.Basic_Screenshot_Code_Java_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
